0cbc50e768c7f22c414f8fbc6fe4297ec3c6c2c9
Júlia Rodrigues <juliardgss@gmail.com>
2020-01-17 21:37:20 -0300

    configurando socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const routes = require('./routes')
+const http = require('http')
+const { setupWebsocket } = require('./websocket')
 
 const app = express()
+const server = http.Server(app)
+
+setupWebsocket(server)
 
 mongoose.connect('mongodb+srv://semana10:semana10@cluster0-bn0pi.mongodb.net/semana10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -15,4 +20,4 @@ app.use(cors())
 app.use(express.json()) //sempre primeiro
 app.use(routes)
 
-app.listen(3333)
+server.listen(3333)
--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -4,6 +4,7 @@ import MapView, { Marker, Callout } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons'
 import api from '../services/api'
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket'
 
 function Main({ navigation }) {
 
@@ -34,6 +35,23 @@ function Main({ navigation }) {
         loadInitialPosition()
     }, [])
 
+    useEffect(() => {
+        subscribeToNewDevs(dev => setDevs([...devs, dev]))
+    }, [devs])
+
+    function setupWebsocket() {
+
+        disconnect()
+
+        const { latitude, longitude } = currentRegion
+
+        connect(
+            latitude,
+            longitude,
+            techs,
+        )
+    }
+
     async function loadDevs() {
         const { latitude, longitude } = currentRegion
         const response = await api.get('/search', {
@@ -43,8 +61,8 @@ function Main({ navigation }) {
                 techs,
             }
         })
-        console.log(response)
         setDevs(response.data.devs)
+        setupWebsocket()
     }
 
     function handleRegionChanged(region) {
